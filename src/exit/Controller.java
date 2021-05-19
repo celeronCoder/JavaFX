0b47f9dfcb96ac14fe19e0986330cb08c413c33d
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-05-19 14:49:16 +0530

    Exit with alert

---
+++ src/exit/Controller.java
@@ -0,0 +1,34 @@
+package exit;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Controller {
+	@FXML
+	private Button logoutBtn;
+	// AnchorPane requires separate @FXML notation
+	@FXML
+	private AnchorPane main;
+
+	private Stage stage;
+
+	public void logout(ActionEvent event) {
+
+		Alert exit_alert = new Alert(Alert.AlertType.CONFIRMATION);
+		exit_alert.setTitle("Logout");
+		exit_alert.setHeaderText("You are about to logout");
+		exit_alert.setContentText("Are you sure you want to exit.");
+
+		// only exit if the alert is opted OK
+		if (exit_alert.showAndWait().get() == ButtonType.OK) {
+			stage = (Stage)main.getScene().getWindow();
+			System.out.println("You successfully logged out of the window");
+			stage.close();
+		}
+	}
+}
---
+++ src/exit/Main.java
@@ -0,0 +1,50 @@
+package exit;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	// to show alert when hit the close button or Alt+F4
+	public void exit(Stage stage) {
+		Alert exit_alert = new Alert(Alert.AlertType.CONFIRMATION);
+		exit_alert.setTitle("Logout");
+		exit_alert.setHeaderText("You are about to logout");
+		exit_alert.setContentText("Are you sure you want to exit.");
+
+		// only exit if the alert is opted OK
+		if (exit_alert.showAndWait().get() == ButtonType.OK) {
+			System.out.println("You successfully logged out of the window");
+			stage.close();
+		}
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		try {
+			Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Main.fxml")));
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.setTitle("Exiting the GUI");
+			stage.show();
+			stage.setOnCloseRequest(event -> {
+				event.consume();
+				exit(stage);
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
