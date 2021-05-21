8d79a5e1f5896c32a047bb714f2e02c9b02c321a
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-05-21 19:53:29 +0530

    Slider Done

---
+++ src/Slider/Controller.java
@@ -0,0 +1,31 @@
+package Slider;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+	@FXML
+	private Slider temperatureSlider;
+	@FXML
+	private Label temp;
+
+	private int temperature;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		temperatureSlider.valueProperty().addListener(new ChangeListener<Number>() {
+			@Override
+			public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+				temperature = (int) temperatureSlider.getValue();
+				temp.setText(Integer.toString(temperature)+ "C");
+			}
+		});
+	}
+}
